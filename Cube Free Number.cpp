11816beb319225d7d7a3a10a56e1cb841876e148
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 17:23:27 +0700

    Trying to hone in on the problem

--- Cube Free Number.cpp
+++ Cube Free Number.cpp
@@ -2,28 +2,30 @@
 #include <math.h>
 int main ()
 {
-	int num, i, j, pos = 0, check, cube;
+	int num, i, j, pos = 1, check, cube = 1;
 	scanf("%d", &num);
 	
 	for(i=1; i<num; i++)
 	{
 		for(j=1; j<i; j++)
 		{
-			check = pow(j*1.0,3);
+			if(pow(j*1.0,3)<=i)
+				check = pow(j*1.0,3);
+			printf("Check for %d = %d\n", i, check);
 			if(i%check == 0)
 			{
 				cube = 0;
 			} else {
-				pos++;
 				cube = 1;
 			}
 		}
+		pos++;
 	}
 	
 	if(cube == 1)
 	{
 		printf("%d", pos);
 	} else {
-		printf("Not Cube Free");
+		printf("Not Cube Free.");
 	}
 }
