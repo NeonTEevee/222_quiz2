e87f40f9a264a1023895ed5b66d46dde790c86b0
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 18:11:01 +0700

    Finished the item

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,23 +3,33 @@
 #include <string.h>
 int main ()
 {
-	int i, j, x, y, left, right, palin = 0, check, value[8], digit;
+	int i, j, x, y, left, right, palin = 0, check, value[8];
 	char num[8];
 	scanf("%s", num);
 	
 	for(i=atoi(num); i>0; i--)
 	{
-		digit = 0;
+		check = 1;
 		x = 0;
 		y = 1;
 		for(j=10; j<=i*10; j*=10)
 		{
 			value[x] = (i%j)/y;
 			x++;
-			digit++;
 			y*=10;
 		}
-		right = value[0];
-		left = value[x-1];
+		left = x-1;
+		right = 0;
+		while(left >= right)
+		{
+			if(value[left] != value[right])
+				check = 0;
+			left--;
+			right++;
+		}
+		if(check == 1)
+			palin++;
 	}
+	
+	printf("%d", palin);
 }
