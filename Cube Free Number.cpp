1bac61b4d9a167d4827066a31e8b0e2706013072
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 17:31:15 +0700

    Figured out the problem

--- Cube Free Number.cpp
+++ Cube Free Number.cpp
@@ -2,14 +2,14 @@
 #include <math.h>
 int main ()
 {
-	int num, i, j, pos = 1, check, cube = 1;
+	int num, i, j, pos = 0, check, cube = 1;
 	scanf("%d", &num);
 	
 	for(i=1; i<=num; i++)
 	{
-		for(j=1; j<=i; j++)
+		for(j=2; j<=i; j++)
 		{
-			if(pow(j*1.0,3)<=i && j>1) {
+			if(pow(j*1.0,3)<=i) {
 				check = pow(j*1.0,3);
 			} else {
 				check = num+1;
@@ -18,11 +18,13 @@ int main ()
 			if(i%check == 0)
 			{
 				cube = 0;
+				j = num+1;
 			} else {
 				cube = 1;
 			}
 		}
-		pos++;
+		if(cube != 0)
+			pos++;
 	}
 	
 	if(cube == 1)
