31f0a37ddf82de95bc8028e54885ecdc78eb4bd7
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 17:17:57 +0700

    Trying to fix things

--- Cube Free Number.cpp
+++ Cube Free Number.cpp
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <math.h>
 int main ()
 {
+	int num, i, j, pos = 0, check, cube;
+	scanf("%d", &num);
 	
+	for(i=1; i<num; i++)
+	{
+		for(j=1; j<i; j++)
+		{
+			check = pow(j*1.0,3);
+			if(i%check == 0)
+			{
+				cube = 0;
+			} else {
+				pos++;
+				cube = 1;
+			}
+		}
+	}
+	
+	if(cube == 1)
+	{
+		printf("%d", pos);
+	} else {
+		printf("Not Cube Free");
+	}
 }
