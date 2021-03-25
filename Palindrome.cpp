621b62a26839818c8dec5cafb31dab26d212cede
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 18:02:36 +0700

    Thinking still

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -3,12 +3,23 @@
 #include <string.h>
 int main ()
 {
-	int i, left, right, palin = 0, check, value[8];
+	int i, j, x, y, left, right, palin = 0, check, value[8], digit;
 	char num[8];
 	scanf("%s", num);
 	
 	for(i=atoi(num); i>0; i--)
 	{
-		
+		digit = 0;
+		x = 0;
+		y = 1;
+		for(j=10; j<=i*10; j*=10)
+		{
+			value[x] = (i%j)/y;
+			x++;
+			digit++;
+			y*=10;
+		}
+		right = value[0];
+		left = value[x-1];
 	}
 }
