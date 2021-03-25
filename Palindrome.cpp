6045e84edd7a33e2eaf2feb3ed51dce62617c08c
NeonTEevee <78305852+NeonTEevee@users.noreply.github.com>
2021-03-25 17:41:25 +0700

    Trying to think about how to go about doing this

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <iostream>
 int main ()
 {
-	int num, i, left, right, palin;
+	int num, i, left, right, palin, len;
 	char check[8];
 	scanf("%d", &num);
 	
-	
+	for(i=1; i<=num; i++)
+	{
+		len = strlen(atoi(num));
+	}
 }
